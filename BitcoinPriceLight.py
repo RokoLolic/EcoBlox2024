7ffaa1ea73377474016b89da3512b380967c2cf4
RokoLolic <73552688+RokoLolic@users.noreply.github.com>
2024-04-07 13:54:10 +0200

    Directions and Description comments in BitcoinPriceLight.py

--- BitcoinPriceLight.py
+++ BitcoinPriceLight.py
@@ -1,3 +1,12 @@
+#BitcoinPriceLight is a Python script that changes the colors of your Govee lights based on if Bitcoin value is growing or falling
+#Green if it's growing, Red if falling. 
+#Interval cannot be too small because then it won't catch the changes and it may cause to go over the rate limit of 10 per minute for govee api 
+#libraries used: https://github.com/LaggAt/python-govee-api https://pypi.org/project/bitcoin-value/
+#you need a govee api key for it by going in your phone app
+
+#Use the script by donwloading it and then in command line typing
+#python3 BitcoinPriceLight.py --api-key {{your api key}}
+
 import argparse
 import asyncio
 from bitcoin_value import currency
